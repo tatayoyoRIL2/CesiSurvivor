16c29a79ab90167508a665280662a06a38d7d39f
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2020-03-20 11:47:47 +0100

    [Back] ADD : connexion à la bdd mariadb Ref #6

--- CesiSurvivor_Back/src/cesisurvivor_api.go
+++ CesiSurvivor_Back/src/cesisurvivor_api.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var db *gorm.DB
+var err error
 
 func main() {
-	fmt.Println("Hello world")
+	// open database
+	db, err = gorm.Open("mysql", "root:0071@tcp(127.0.0.1:3306)/CesiSurvivor?charset=utf8&parseTime=True")
+	if err != nil {
+		log.Println("Connection Failed to Open")
+	} else {
+		log.Println("Connection Established")
+	}
+
 }
