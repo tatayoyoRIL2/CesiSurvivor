889bd48ff2154ab33ed74ae88d6b1ad68aa92458
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2020-04-06 16:02:42 +0200

    [Back] ADD: users dans le déploiement de la bdd et mise à jour des logins bdd

--- CesiSurvivor_Back/deployments/init_database.sh
+++ CesiSurvivor_Back/deployments/init_database.sh
@@ -2,19 +2,25 @@
 set -e
 
 # all constants
-DB_USER='omega';
-DB_PASSWD='Hb6w7';
-DB_NAME='CesiSurvivor';
-HOST='127.0.0.1';
+DB_USER='omega'
+DB_PASSWD='Hb6w7'
+DB_NAME='CesiSurvivor'
+TABLE_USERS='users'
 
-# database connection and create database
-mysql --user=${DB_USER} --host=${HOST} --password=${DB_PASSWD} <<EOF
+# database connection and commands
+mysql --user=${DB_USER} --host=127.0.0.1 --password=${DB_PASSWD} <<EOF
 
 DROP DATABASE IF EXISTS ${DB_NAME};
-
 create database ${DB_NAME} default character set utf8 default collate utf8_bin;
-
 use ${DB_NAME};
+
+DROP TABLE IF EXISTS ${TABLE_USERS};
+CREATE TABLE IF NOT EXISTS ${TABLE_USERS} (
+    idUser INT AUTO_INCREMENT primary key NOT NULL,
+    username VARCHAR(255) NOT NULL
+);
+INSERT INTO ${TABLE_USERS} (\`idUser\`, \`username\`) VALUES (NULL, "omega");
+INSERT INTO ${TABLE_USERS} (\`idUser\`, \`username\`) VALUES (NULL, "yoyotata");
 
 exit
 EOF
