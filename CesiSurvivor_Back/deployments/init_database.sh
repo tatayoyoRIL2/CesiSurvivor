7b7d07ee47ed9d8cf9204c61084fdeb770bf661a
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2020-04-06 16:29:07 +0200

    [Back] ADD: questions dans le déploiement de la bdd

--- CesiSurvivor_Back/deployments/init_database.sh
+++ CesiSurvivor_Back/deployments/init_database.sh
@@ -7,6 +7,7 @@ DB_PASSWD='Hb6w7'
 DB_NAME='CesiSurvivor'
 TABLE_USERS='users'
 TABLE_SCORES='scores'
+TABLE_QUESTIONS='questions'
 
 # database connection and commands
 mysql --user=${DB_USER} --host=127.0.0.1 --password=${DB_PASSWD} <<EOF
@@ -31,6 +32,17 @@ CREATE TABLE IF NOT EXISTS ${TABLE_SCORES} (
 );
 INSERT INTO ${TABLE_SCORES} (\`idScore\`, \`score\`, \`idUser\`) VALUES (NULL, 7, 1);
 INSERT INTO ${TABLE_SCORES} (\`idScore\`, \`score\`, \`idUser\`) VALUES (NULL, 9, 2);
+
+DROP TABLE IF EXISTS ${TABLE_QUESTIONS};
+CREATE TABLE IF NOT EXISTS ${TABLE_QUESTIONS} (
+    idQuestion INT AUTO_INCREMENT primary key NOT NULL,
+    label VARCHAR(255) NOT NULL,
+    answerList VARCHAR(255) NOT NULL
+);
+INSERT INTO ${TABLE_QUESTIONS} (\`idQuestion\`, \`label\`, \`answerList\`) VALUES (NULL, "A", "[1, 2, 3, 4]");
+INSERT INTO ${TABLE_QUESTIONS} (\`idQuestion\`, \`label\`, \`answerList\`) VALUES (NULL, "B", "[5, 6, 7, 8]");
+INSERT INTO ${TABLE_QUESTIONS} (\`idQuestion\`, \`label\`, \`answerList\`) VALUES (NULL, "C", "[9, 10, 11, 12]");
+INSERT INTO ${TABLE_QUESTIONS} (\`idQuestion\`, \`label\`, \`answerList\`) VALUES (NULL, "B", "[13, 14, 15, 16]");
 
 exit
 EOF
