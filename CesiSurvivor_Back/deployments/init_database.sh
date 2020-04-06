977baef4e4193c6bff3094e2ca0f1b36768ae373
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2020-04-06 11:07:37 +0200

    [Back] UPD: la création et insert des tables se fait par l'api go et plus en bash.
    Author:    omegaBionic <44264108+omegaBionic@users.noreply.github.com>
    
    Author:    omegaBionic <44264108+omegaBionic@users.noreply.github.com>

--- CesiSurvivor_Back/deployments/init_database.sh
+++ CesiSurvivor_Back/deployments/init_database.sh
@@ -10,9 +10,8 @@ fi
 DB_USER='admin';
 DB_PASSWD='0071';
 DB_NAME='CesiSurvivor';
-TABLE='tests';
 
-# database connection and commands
+# database connection and create database
 mysql --user=${DB_USER} --password=${DB_PASSWD} <<EOF
 
 DROP DATABASE IF EXISTS ${DB_NAME};
@@ -20,20 +19,6 @@ DROP DATABASE IF EXISTS ${DB_NAME};
 create database ${DB_NAME} default character set utf8 default collate utf8_bin;
 
 use ${DB_NAME};
-
-DROP TABLE IF EXISTS ${TABLE};
-
-CREATE TABLE IF NOT EXISTS ${TABLE} (
-    id INT AUTO_INCREMENT primary key NOT NULL,
-    day VARCHAR(255) NOT NULL,
-    time VARCHAR(255) NOT NULL,
-    msg VARCHAR(255) NOT NULL
-);
-
-INSERT INTO ${TABLE} (\`id\`, \`day\`, \`time\`, \`msg\`) VALUES (NULL, "monday", "16:47", "Hey bro!");
-INSERT INTO ${TABLE} (\`id\`, \`day\`, \`time\`, \`msg\`) VALUES (NULL, "monday", "16:48", "Hey bro!");
-INSERT INTO ${TABLE} (\`id\`, \`day\`, \`time\`, \`msg\`) VALUES (NULL, "monday", "16:49", "Hey bro!");
-INSERT INTO ${TABLE} (\`id\`, \`day\`, \`time\`, \`msg\`) VALUES (NULL, "monday", "16:50", "Hey bro!");
 
 exit
 EOF
