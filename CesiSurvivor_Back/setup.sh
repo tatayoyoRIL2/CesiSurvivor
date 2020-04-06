57017bec20bbec791840fd921a0fea8d596803ae
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2020-04-06 11:32:52 +0200

    [Back] ADD: bash setup

--- CesiSurvivor_Back/setup.sh
+++ CesiSurvivor_Back/setup.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -e
 
+# go setup
 go get github.com/gorilla/mux
 go get github.com/jinzhu/gorm
 go get github.com/go-sql-driver/mysql
+
+# bash setup
+sudo apt-get update
+sudo apt-get install mysql-client -y
