b7c9186ff12259db3a628b445959694875c40e32
omegaBionic <44264108+omegaBionic@users.noreply.github.com>
2020-04-06 11:30:48 +0200

    [Back] UPD: on peut éxécuter le script go setup en root

--- CesiSurvivor_Back/setup.sh
+++ CesiSurvivor_Back/setup.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -e
 
-if [ "$(id -u)" = "0" ]; then
-echo "Do not execute this script as root!"
-exit 1
-fi
-
-
 go get github.com/gorilla/mux
 go get github.com/jinzhu/gorm
 go get github.com/go-sql-driver/mysql
